45eda351d63f90472e2ed72807fa5dc4bb5a5917
Dzmitry Piskunou <pinkskyinyoureyes@gmail.com>
2023-02-23 23:55:07 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-
-// You should implement your task here.
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [10, 11, 12],
+  [13, 14, 15]
+]
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (arguments[0] === undefined) {
+    return []
+  } else if (arguments[0].length === 0) {
+    return []
+  } else {
+    for(let i = 0; i < matrix.length; i++){
+      if (i % 2) {
+        matrix[i].reverse()
+      }
+    }
+
+    let newMatrix = matrix.join().split(',')
+  
+    for(let i = 0; i < newMatrix.length; i++){
+      newMatrix[i] = Number(newMatrix[i])
+    }
+
+    return newMatrix
+  }
 }
